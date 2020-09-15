816fb0242c12b0764ad681a5e80b789f00047684
Qinxin Zhang <qinxin.zhang@lotusflare.com>
2020-09-15 16:43:45 +0800

    add loading bar in webview

--- lib/pages/detail_pages/my_webview.dart
+++ lib/pages/detail_pages/my_webview.dart
@@ -2,21 +2,45 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class MyWebview extends StatelessWidget {
+class MyWebview extends StatefulWidget {
   String _title;
   String _url;
 
   MyWebview(this._title, this._url);
 
   @override
+  State<StatefulWidget> createState() {
+    return MyWebviewState();
+  }
+}
+
+class MyWebviewState extends State<MyWebview> {
+  bool _isLoadingPage = true;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(_title),
-        ),
-        body: WebView(
-          initialUrl: "https://www.wanandroid.com" + _url,
-          javascriptMode: JavascriptMode.unrestricted,
-        ));
+      appBar: AppBar(
+        title: Text(widget._title),
+      ),
+      body: Stack(
+        children: <Widget>[
+          WebView(
+            initialUrl: "https://www.wanandroid.com" + widget._url,
+            javascriptMode: JavascriptMode.unrestricted,
+            onPageFinished: (finish) {
+              setState(() {
+                _isLoadingPage = false;
+              });
+            },
+          ),
+          _isLoadingPage
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Stack(),
+        ],
+      ),
+    );
   }
 }
